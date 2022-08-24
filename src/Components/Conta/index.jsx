d332ae8760482d3689f734047dc913bcf1b58e57
Alexc0217 <alex.costa.moura@outlook.com>
2022-08-24 14:08:50 -0300

    styled components project

---
+++ src/Components/Conta/index.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import privado from "../../assets/images/privado.svg";
+import olho_icone from "../../assets/images/olho.svg";
+import dinheiro from "../../assets/images/dinheiro.svg";
+
+import { Botao, Box, Detalhe, Saldo, IconeTema } from "../UI";
+
+const IconeMargin = styled(IconeTema)`
+  marginTop: "2px"
+`
+
+const SaldoStyle = styled(Saldo)`
+  fontSize: "26px"
+  padding: "20px 0"
+`
+
+const Conta = () => {
+  const [toggleState, untoggle] = useState(true);
+
+  const toggleHandler = () => {
+    untoggle((toggleState) => !toggleState);
+  };
+
+  return (
+    <Box>
+      <h2>Conta</h2>
+      <div style={{ fontSize: "26px", padding: "20px 0" }}>
+        Saldo disponível{" "}
+        <span>
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
+        </span>
+        {toggleState ? (
+          <Saldo>
+            <Detalhe>R$</Detalhe> 0,00{" "}
+          </Saldo>
+        ) : 
+          <Saldo>
+            <Detalhe>R$</Detalhe> x,xx{" "}
+          </Saldo>
+        }
+      </div>
+
+      <Botao onClick={toggleHandler}>
+        <IconeTema
+          src={toggleState ? privado : olho_icone}
+          alt="Privacidade do Saldo"
+        />
+      </Botao>
+    </Box>
+  );
+};
+
+export default Conta;
